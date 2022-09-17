0bcc12fec4fd62fe65269e9a438d00df0c7b799a
mininu492 <112704974+mininu492@users.noreply.github.com>
2022-09-18 05:07:01 +0900

    스파인 플레이어 이동구현
    샘플 플레이어와 같은 동작 형태 (4방향 이동, Idle모션 1개)로 구현 완료
    
    -기존 프로젝트, 코드 건드림x
    -유니티-스파인 런타임 설치(4.1버전)
    -Asset/Spine 폴더 생성됨, 폴더 내에서 작업중
    
    *애니메이션 트리가 아닌 배열+애니메이션 모션 클립 사용
    -> 추후 가능시 애니메이터/트리사용으로 옮길예정

---
+++ Assets/Spine/Kimmi/SpinePlayerController.cs
@@ -0,0 +1,118 @@
+using Spine.Unity;
+using UnityEngine;
+
+public class SpinePlayerController : MonoBehaviour
+{
+
+    public float speed = 4.0f;
+    public SkeletonAnimation SkeletonAnimation;
+    public AnimationReferenceAsset[] animClip;
+
+    public enum AnimState
+    {
+        Idle_E = 0,
+        Idle_N = 1,
+        Idle_S = 2,
+
+        Walk_E = 3,
+        Walk_N = 4,
+        Walk_S = 5
+    }
+
+    private AnimState _animState;
+    private string CurrentAnimation;
+
+    private Rigidbody2D _characterRigidbody;
+    private Vector2 _movement;
+    //private Vector2 _formerMovement;
+
+    private void Start()
+    {
+        // 구독 신청! KeyAction이 Invoke 되면 호출할 함수! (중복을 막기위해 빼준 후 추가)
+        Managers.Input.KeyAction -= OnKeyboard;
+        Managers.Input.KeyAction += OnKeyboard;
+
+        _characterRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() => UpdateState();
+
+    private void FixedUpdate() =>  OnKeyboard();
+
+    private void OnKeyboard()
+    {
+        _movement.x = Input.GetAxisRaw("Horizontal");
+        _movement.y = Input.GetAxisRaw("Vertical");
+
+        _movement.Normalize();
+
+        _characterRigidbody.velocity = _movement * speed;
+    }
+
+    private void UpdateState()
+    {
+        transform.localScale = new Vector3(1f, 1f, 1f);
+
+        if (_movement.x > 0)
+        {
+            _animState = AnimState.Walk_E;
+        }
+        else if (_movement.x < 0)
+        {
+            _animState = AnimState.Walk_E;
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+        else if (_movement.y > 0)
+        {
+            _animState = AnimState.Walk_N;
+        }
+        else if (_movement.y < 0)
+        {
+            _animState = AnimState.Walk_S;
+        }
+        else
+        {
+            if (CurrentAnimation == null)
+            {
+                _animState = AnimState.Idle_S;
+                return;
+            }
+
+            //작동하지 않는 코드... 이전에 움직이고있던 방향대로 Idle모션을 다르게 주고 싶음
+            //if(_formerMovement.x!=0)
+            //{
+            //    _animState = AnimState.Idle_E;
+
+            //}else if (_formerMovement.y>0)
+            //{
+            //    _animState = AnimState.Idle_N;
+            //}
+            else
+            {
+                _animState = AnimState.Idle_S;
+            }
+        }
+
+        SetCurrentAnimation(_animState);
+        //_formerMovement = new Vector2(_movement.x, _movement.y);
+    }
+
+    private void AsyncAnimation(AnimationReferenceAsset animClip, bool loop, float timeScale)
+    {
+        //이미 동일한 이름의 애니메이션이 재생중이라면 다시 재생시키지 않음
+        if (animClip.name.Equals(CurrentAnimation))
+        {
+            return;
+        }
+
+        SkeletonAnimation.state.SetAnimation(0, animClip, loop).TimeScale = timeScale;
+        CurrentAnimation = animClip.name;
+    }
+
+    private void SetCurrentAnimation(AnimState state)
+    {
+        AsyncAnimation(animClip[(int)state], true, 1f);
+    }
+
+
+}
