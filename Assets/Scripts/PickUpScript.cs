222731f1859f20ec1e87d763770bbec68881579a
Mossworm <55495046+Mossworm@users.noreply.github.com>
2022-10-11 20:39:20 +0900

    타이머 수정
    ㅇㅇ

--- Assets/Scripts/PickUpScript.cs
+++ Assets/Scripts/PickUpScript.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
-using UnityEditor.PackageManager;
 using UnityEngine.Serialization;
 
 public class PickUpScript : MonoBehaviour
